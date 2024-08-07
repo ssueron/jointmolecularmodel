1b8a9b89ec9a6185b69195e23e4ff9307038069b
derekvantilborg <dwvantilborg@gmail.com>
2024-08-07 15:13:54 +0200

    compute all important distance metrics

--- experiments/2.2_distance_metrics.py
+++ experiments/2.2_distance_metrics.py
@@ -2,9 +2,12 @@
 from jcm.training_logistics import get_all_dataset_names, load_dataset_df
 from constants import ROOTDIR
 import os
-
-
-
+from os.path import join as ospj
+import pandas as pd
+from cheminformatics.molecular_similarity import tani_sim_to_train, mean_cosine_cats_to_train, \
+    substructure_sim_to_train, applicability_domain_kNN, applicability_domain_SDC
+from cheminformatics.complexity import molecular_complexity
+from tqdm.auto import tqdm
 
 
 if __name__ == '__main__':
@@ -12,6 +15,44 @@ if __name__ == '__main__':
     # move to root dir
     os.chdir(ROOTDIR)
 
-    all_datasets = get_all_dataset_names()
+    all_dataset_names = get_all_dataset_names()
 
+    for dataset_name in tqdm(all_dataset_names):
 
+        # load the data and get rid of all columns that might be in there
+        df = load_dataset_df(dataset_name)
+        df = df.loc[:, ['smiles', 'y', 'cluster', 'split']]
+
+        # get the smiles strings
+        train_smiles = df[df['split'] == 'train'].smiles.tolist()
+        all_smiles = df.smiles.tolist()
+
+        # ECFP Tanimoto similarity
+        tani = tani_sim_to_train(all_smiles, train_smiles, radius=2, nbits=2048, scaffold=False)
+        df['Tanimoto_to_train'] = tani
+
+        tani_scaf = tani_sim_to_train(all_smiles, train_smiles, radius=2, nbits=2048, scaffold=True)
+        df['Tanimoto_scaffold_to_train'] = tani_scaf
+
+        # Cats Cosine similarity
+        cats_cos = mean_cosine_cats_to_train(all_smiles, train_smiles)
+        df['Cats_cos'] = cats_cos
+
+        # fractional MSC
+        frMSC = substructure_sim_to_train(all_smiles, train_smiles, scaffold=False)
+        df['frMSC'] = frMSC
+
+        # Compute "complexity" of molecules
+        complex = [molecular_complexity(smi) for smi in tqdm(all_smiles)]
+        df = pd.concat((df, pd.DataFrame(complex)), axis=1)
+
+        # KNN OOD metric; 10.1021/acs.chemrestox.9b00498
+        knn_ad = applicability_domain_kNN(all_smiles, train_smiles, k=10, sim_cutoff=0.25)
+        df['knn_ad'] = knn_ad
+
+        # Sum of distance-weighted contributions; 10.1021/acs.jcim.8b00597
+        sdc_ad = applicability_domain_SDC(all_smiles, train_smiles)
+        df['sdc_ad'] = sdc_ad
+
+        # write to file
+        df.to_csv(ospj('data', 'datasets_with_metrics', f'{dataset_name}.csv'), index=False)
