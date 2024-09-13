b0ace90da7aea184a52a9f63db00a3c0bb92c20c
derekvantilborg <dwvantilborg@gmail.com>
2024-09-13 15:25:50 +0200

    inference support

--- experiments/4.5_jvae.py
+++ experiments/4.5_jvae.py
@@ -6,10 +6,13 @@ September 2024
 """
 
 import os
+import random
 from os.path import join as ospj
 import argparse
 from itertools import batched
+from collections import defaultdict
 
+import numpy as np
 import pandas as pd
 from jcm.config import finish_experiment
 from jcm.training import Trainer
@@ -20,6 +23,9 @@ from jcm.config import init_experiment, load_settings
 from jcm.callbacks import jvae_callback
 import torch
 from sklearn.model_selection import train_test_split, ParameterGrid
+from jcm.utils import logits_to_pred
+from cheminformatics.encoding import strip_smiles, probs_to_smiles
+from cheminformatics.eval import smiles_validity, reconstruction_edit_distance, plot_molecular_reconstruction
 
 
 def write_job_script(dataset_names: list[str], out_paths: list[str] = 'results', experiment_name: str = "cats_mlp",
@@ -93,11 +99,11 @@ def jvae_merge_pretrained_configs(default_config_path: str, vae_config_path: str
     return default_config
 
 
-def find_seeds(dataset: str) -> list[int]:
+def find_seeds(dataset: str) -> tuple[int]:
 
     df = pd.read_csv(ospj("data", "best_model", "smiles_mlp", dataset, 'results_preds.csv'))
 
-    return set(df.seed)
+    return tuple(set(df.seed))
 
 
 def get_mlp_state_dict(mlp_model_path: str, config):
@@ -171,7 +177,10 @@ def init_jvae(jvae_config, best_vae_weights_path: str, dataset: str, seed: int):
     return model
 
 
-def run_models(hypers: dict, out_path: str, experiment_name: str, dataset: str):
+def run_models(hypers: dict, out_path: str, experiment_name: str, dataset: str, save_best_model: bool = True):
+
+    best_val_losses = []
+    all_results = []
 
     # 1. Setup config
     jvae_config = setup_config(default_config_path=DEFAULT_SETTINGS_PATH, best_vae_config_path=BEST_VAE_CONFIG_PATH,
@@ -180,7 +189,7 @@ def run_models(hypers: dict, out_path: str, experiment_name: str, dataset: str):
 
     # 2. Find which seeds were used during pretraining. Train a model for every cross-validation split/seed
     seeds = find_seeds(dataset)
-    for seed in seeds:
+    for seed in seeds[:3]:  # TODO remove this
         # 2.2. get the data belonging to a certain cross-validation split/seed
         train_dataset, val_dataset, test_dataset, ood_dataset = load_data_for_seed(dataset, seed)
 
@@ -197,8 +206,81 @@ def run_models(hypers: dict, out_path: str, experiment_name: str, dataset: str):
             T.set_callback('on_batch_end', jvae_callback)
         T.run()
 
-        # 2.5. save model
-        model.save_weights(ospj(out_path, f"model_{seed}.pt"))
+        # 2.5. save model and training history
+        if save_best_model:
+            model.save_weights(ospj(out_path, f"model_{seed}.pt"))
+        if out_path is not None:
+            T.get_history(ospj(out_path, f"training_history_{seed}.csv"))
+
+            all_results.append(perform_inference(model, train_dataset, test_dataset, ood_dataset, seed))
+            pd.concat(all_results).to_csv(ospj(out_path, 'results_preds.csv'), index=False)
+
+        best_val_losses.append(min(T.history['val_loss']))
+
+    return sum(best_val_losses)/len(best_val_losses)
+
+
+def reconstruct_smiles(logits_N_S_C, true_smiles: list[str]):
+
+    # reconstruction
+    designs = probs_to_smiles(logits_N_S_C)
+
+    # Clean designs
+    designs_clean = strip_smiles(designs)
+    validity, reconstructed_smiles = smiles_validity(designs_clean, return_invalids=True)
+
+    edit_distances = []
+    for true_smi, smi in zip(true_smiles, reconstructed_smiles):
+        edist = reconstruction_edit_distance(true_smi, smi) if smi is not None else None
+        edit_distances.append(edist)
+
+    return reconstructed_smiles, edit_distances, validity
+
+
+def perform_inference(model, train_dataset, test_dataset, ood_dataset, seed):
+
+    # perform predictions on all splits
+    logits_N_K_C_token_train, y_logits_N_K_C_train, molecule_reconstruction_losses_train, _, y_train, smiles_train = model.predict(train_dataset)
+    logits_N_K_C_token_test, y_logits_N_K_C_test, molecule_reconstruction_losses_test, _, y_test, smiles_test = model.predict(test_dataset)
+    logits_N_K_C_token_ood, y_logits_N_K_C_ood, molecule_reconstruction_losses_ood, _, y_ood, smiles_ood = model.predict(ood_dataset)
+
+    # convert y hat logits into binary predictions
+    y_hat_train, y_unc_train = logits_to_pred(y_logits_N_K_C_train, return_binary=True)
+    y_hat_test, y_unc_test = logits_to_pred(y_logits_N_K_C_test, return_binary=True)
+    y_hat_ood, y_unc_ood = logits_to_pred(y_logits_N_K_C_ood, return_binary=True)
+
+    # reconstruct the smiles
+    reconst_smiles_train, edit_dist_train, validity_train = reconstruct_smiles(logits_N_K_C_token_train, smiles_train)
+    reconst_smiles_test, edit_dist_test, validity_test = reconstruct_smiles(logits_N_K_C_token_test, smiles_test)
+    reconst_smiles_ood, edit_dist_ood, validity_ood = reconstruct_smiles(logits_N_K_C_token_ood, smiles_ood)
+
+    # Put the predictions in a dataframe
+    train_results_df = pd.DataFrame({'seed': seed, 'split': 'train', 'smiles': smiles_train,
+                                     'reconstructed_smiles': reconst_smiles_train,
+                                     'edit_distance': edit_dist_train,
+                                     'reconstruction_loss': molecule_reconstruction_losses_train.cpu(),
+                                     'y': y_train.cpu(), 'y_hat': y_hat_train.cpu(),
+                                     'y_unc': y_unc_train.cpu()})
+
+    # Put the predictions in a dataframe
+    test_results_df = pd.DataFrame({'seed': seed, 'split': 'train', 'smiles': smiles_test,
+                                     'reconstructed_smiles': reconst_smiles_test,
+                                     'edit_distance': edit_dist_test,
+                                     'reconstruction_loss': molecule_reconstruction_losses_test.cpu(),
+                                     'y': y_test.cpu(), 'y_hat': y_hat_test.cpu(),
+                                     'y_unc': y_unc_test.cpu()})
+
+    # Put the predictions in a dataframe
+    ood_results_df = pd.DataFrame({'seed': seed, 'split': 'train', 'smiles': smiles_ood,
+                                     'reconstructed_smiles': reconst_smiles_ood,
+                                     'edit_distance': edit_dist_ood,
+                                     'reconstruction_loss': molecule_reconstruction_losses_ood.cpu(),
+                                     'y': y_ood.cpu(), 'y_hat': y_hat_ood.cpu(),
+                                     'y_unc': y_unc_ood.cpu()})
+
+    results_df = pd.concat((train_results_df, test_results_df, ood_results_df))
+
+    return results_df
 
 
 if __name__ == '__main__':
@@ -212,9 +294,10 @@ if __name__ == '__main__':
     BEST_VAE_PATH = ospj('data', 'best_model', 'pretrained', 'vae', 'weights.pt')
     BEST_VAE_CONFIG_PATH = ospj('data', 'best_model', 'pretrained', 'vae', 'config.yml')
 
-    SEARCH_SPACE = {'lr': [3e-4, 3e-5, 3e-6],
-                    'mlp_loss_scalar': [0.1, 0.5, 1],
-                    'freeze_encoder': [True, False]}
+    SEARCH_SPACE = {'lr': [3e-4],  # [3e-4, 3e-5, 3e-6]
+                    'mlp_loss_scalar': [0.1],  # [0.1, 0.5, 1]
+                    'freeze_encoder': [True, False],  # [True, False]
+                    }
     hyper_grid = ParameterGrid(SEARCH_SPACE)
 
     dataset = "CHEMBL204_Ki"
@@ -222,12 +305,26 @@ if __name__ == '__main__':
     out_path = ospj('results/jvae', dataset)
 
     # Train models in 10-fold cross validation over the whole hyperparameter space.
+    hyper_performance = defaultdict(list)
     for exp_i, hypers in enumerate(hyper_grid):
 
         # create an experiment-specific out_path and experiment_name
-        _out_path = ospj(out_path, str(exp_i))
         _experiment_name = f"{EXPERIMENT_NAME}_{dataset}_{exp_i}"
 
-        run_models(hypers, out_path=_out_path, experiment_name=_experiment_name, dataset=dataset)
+        mean_val_loss = run_models(hypers, out_path=None, experiment_name=_experiment_name, dataset=dataset,
+                                   save_best_model=False)
+
+        hyper_performance['mean_val_loss'].append(mean_val_loss)
+        hyper_performance['hypers'].append(hypers)
+
+    # Get the best performing hyperparameters
+    best_hypers = hyper_performance['hypers'][np.argmin(hyper_performance['mean_val_loss'])]
+    print(f"\n\nBest hyperparams (val loss of {min(hyper_performance['mean_val_loss']):.4f}) are:\n{best_hypers}\n\n")
+
+    # Train the JVAE model with the best hyperparameters, but now save the models
+    run_models(best_hypers, out_path=out_path, experiment_name=f"{EXPERIMENT_NAME}_{dataset}_best",
+               dataset=dataset, save_best_model=True)
+
+    perform_inference(out_path=out_path)
 
     finish_experiment()
