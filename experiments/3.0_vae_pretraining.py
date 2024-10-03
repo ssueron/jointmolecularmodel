e267eac84af14206d1af52090d38d172768db956
derekvantilborg <dwvantilborg@gmail.com>
2024-10-03 13:16:47 +0200

    got rid of old scripts

--- experiments/3.0_vae_pretraining.py
+++ experiments/3.0_vae_pretraining.py
@@ -3,7 +3,7 @@ our computer cluster, that in turn run this script with a certain set of hyperpa
 
 Derek van Tilborg
 Eindhoven University of Technology
-June 2024
+Sept 2024
 """
 
 import os
@@ -51,9 +51,9 @@ def train_model(config):
     T.run()
 
 
-def write_job_script(experiments: list[int], experiment_name: str = "vae_pretraining",
+def write_job_script(experiments: list[int], out_paths: list[str] = 'results', experiment_name: str = "vae_pretraining",
                      experiment_script: str = "3.0_vae_pretraining.py", partition: str = 'gpu', ntasks: str = '18',
-                     gpus_per_node: str = 1, time: str = "4:00:00") -> None:
+                     gpus_per_node: str = 1, time: str = "120:00:00") -> None:
     """
     :param experiments: list of experiment numbers, e.g. [0, 1, 2]
     """
@@ -70,20 +70,17 @@ def write_job_script(experiments: list[int], experiment_name: str = "vae_pretrai
     lines.append(f'#SBATCH --gpus-per-node={gpus_per_node}\n')
     lines.append(f'#SBATCH --time={time}\n')
     lines.append('\n')
-    lines.append(f'experiment_name="{experiment_name}"\n')
-    lines.append('\n')
     lines.append('project_path="$HOME/projects/JointChemicalModel"\n')
     lines.append(f'experiment_script_path="$project_path/experiments/{experiment_script}"\n')
     lines.append('\n')
-    lines.append('out_path="$project_path/results/$experiment_name"\n')
     lines.append('log_path="$project_path/results/logs"\n')
     lines.append('\n')
     lines.append('source $HOME/anaconda3/etc/profile.d/conda.sh\n')
     lines.append('export PYTHONPATH="$PYTHONPATH:$project_path"\n')
 
-    for i, exp in enumerate(experiments):
+    for i, (exp, out_path) in enumerate(zip(experiments, out_paths)):
         lines.append('\n')
-        lines.append('$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment EX > "$log_path/${experiment_name}_EX.log" &\n'.replace('EX', str(exp)))
+        lines.append('$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o OUT_PATH -experiment EX > "$log_path/XE.log" &\n'.replace('EX', str(exp)).replace('XE', f"{experiment_name}_{exp}").replace('OUT_PATH', out_path))
         lines.append(f'pid{i+1}=$!\n')
 
     lines.append('\n')
@@ -91,10 +88,16 @@ def write_job_script(experiments: list[int], experiment_name: str = "vae_pretrai
         lines.append(f'wait $pid{i+1}\n')
     lines.append('\n')
 
-    outpath = ospj(ROOTDIR, 'experiments', 'jobs', jobname + '.sh')
+    # Move all output files to the project directory
+    for i, out_path in enumerate(out_paths):
+        source = f"$project_path/{out_path}"
+        destination = f"/projects/prjs1021/JointChemicalModel/{os.path.dirname(out_path)}/"
+
+        lines.append(f'cp -r {source} {destination}\n')
+        lines.append(f"if [ $? -eq 0 ]; then\n    rm -rf {source}\nfi\n\n")
 
     # Write the modified lines back to the file
-    with open(outpath, 'w') as file:
+    with open(ospj(ROOTDIR, 'experiments', 'jobs', jobname + '.sh'), 'w') as file:
         file.writelines(lines)
 
 
@@ -102,28 +105,32 @@ if __name__ == '__main__':
 
     # global variables
     DEFAULT_SETTINGS_PATH = "experiments/hyperparams/vae_pretrain_default.yml"
+    EXPERIMENT_NAME = "vae_pretraining"
     SEARCH_SPACE = {'lr': [3e-4, 3e-5, 3e-6],
                     'cnn_out_hidden': [256, 512],
                     'cnn_kernel_size': [6],
                     'cnn_n_layers': [2, 3],
                     'z_size': [128],
-                    'rnn_type': ['gru', 'lstm'],
+                    'rnn_type': ['lstm'],
                     'rnn_hidden_size': [512],
                     'rnn_num_layers': [2, 3],
                     'rnn_dropout': [0.2],
                     'variational_scale': [0.1],
-                    'beta': [0.001],
+                    'beta': [0.1],
                     'grad_norm_clip': [5],
-                    'data_augmentation': [True, False],
-                    'rnn_teacher_forcing': [True]
+                    'data_augmentation': [False],
+                    'rnn_teacher_forcing': [False]
                    }
 
     hyper_grid = ParameterGrid(SEARCH_SPACE)
 
-    # experiment_batches = [i for i in batched(range(len(hyper_grid)), 5)]
+    # experiment_batches = [tuple(str(j) for j in i) for i in batched(range(len(hyper_grid)), 5)]
     # for batch in experiment_batches:
+    #     out_paths = [f"results/{EXPERIMENT_NAME}/{exp}" for exp in batch]
+    #
     #     write_job_script(experiments=batch,
-    #                      experiment_name="vae_pretraining",
+    #                      out_paths=out_paths,
+    #                      experiment_name=EXPERIMENT_NAME,
     #                      experiment_script="3.0_vae_pretraining.py",
     #                      partition='gpu',
     #                      ntasks='18',
@@ -131,7 +138,7 @@ if __name__ == '__main__':
     #                      time="120:00:00"
     #                      )
 
-    # parse script arguments
+    # # parse script arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', help='The path of the output directory', default='results')
     parser.add_argument('-experiment')
@@ -148,7 +155,8 @@ if __name__ == '__main__':
                            'data_augmentation': experiment_hypers['data_augmentation']}
 
     config = init_experiment(config_path=DEFAULT_SETTINGS_PATH, config_dict=experiment_settings,
-                             hyperparameters=experiment_hypers, name=f"vae_pretraining_{experiment}")
+                             hyperparameters=experiment_hypers, name=f"vae_pretraining_{experiment}",
+                             group='vae_pretraining')
 
     print('Experiment config:')
     print(config, '\n')
