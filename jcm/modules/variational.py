e04edfa9ada88a2bef01ccb4c84d8eb9f882a431
derekvantilborg <dwvantilborg@gmail.com>
2024-11-07 15:23:19 +0100

    fixed wrong variable name

--- jcm/modules/variational.py
+++ jcm/modules/variational.py
@@ -30,7 +30,7 @@ class VariationalEncoder(nn.Module):
         self.lin0_mu = nn.Linear(z_size, z_size)
         self.lin0_sigma = nn.Linear(z_size, z_size)
 
-        self.N = torch.distributions.Normal(0, self.prior_std)
+        self.N = torch.distributions.Normal(0, self.sigma_prior)
         self.N.loc = self.N.loc
         self.N.scale = self.N.scale
         self.kl = 0
