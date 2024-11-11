e597d8230ddbebcae99a5873ecaaee1ca757b76d
derekvantilborg <dwvantilborg@gmail.com>
2024-11-11 11:55:33 +0100

    rnn best hypers training job

---
+++ experiments/jobs/rnn_pretraining_0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=rnn_pretraining_0
+#SBATCH --output=/home/tilborgd/projects/JointChemicalModel/results/out/rnn_pretraining_0.out
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --ntasks=18
+#SBATCH --gpus-per-node=1
+#SBATCH --time=120:00:00
+
+experiment_name="rnn_pretraining"
+
+project_path="$HOME/projects/JointChemicalModel"
+experiment_script_path="$project_path/experiments/3.1_rnn_pretraining.py"
+
+out_path="$project_path/results/$experiment_name"
+log_path="$project_path/results/logs"
+
+source $HOME/anaconda3/etc/profile.d/conda.sh
+export PYTHONPATH="$PYTHONPATH:$project_path"
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 0 > "$log_path/${experiment_name}_0.log" &
+pid1=$!
+
+wait $pid1
+
