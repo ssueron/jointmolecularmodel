d3d75cb840ff2c45a5e4a035fe0bad7eed533c55
derekvantilborg <dwvantilborg@gmail.com>
2025-02-24 13:34:29 +0100

    split all screening libraries up into their own script

---
+++ experiments/jobs/library_inference_asinex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=library_inference_asinex
+#SBATCH --output=/home/tilborgd/projects/JointChemicalModel/results/out/library_inference_asinex.out
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --ntasks=18
+#SBATCH --gpus-per-node=1
+#SBATCH --time=120:00:00
+
+project_path="$HOME/projects/JointChemicalModel"
+experiment_script_path="$project_path/experiments/6.1_jmm_inference_asinex.py"
+
+log_path="$project_path/results/logs"
+
+source $HOME/anaconda3/etc/profile.d/conda.sh
+export PYTHONPATH="$PYTHONPATH:$project_path"
+
+echo 'running script'
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path > "$log_path/library_inference_asinex.log"
