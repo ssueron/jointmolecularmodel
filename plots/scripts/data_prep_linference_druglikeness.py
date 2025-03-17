7b8de44fb6dad09e093f18077ce82032a5b1949c
derekvantilborg <dwvantilborg@gmail.com>
2025-03-17 09:17:31 +0100

    Compute drug-likeness for all screening molecules

---
+++ plots/scripts/data_prep_linference_druglikeness.py
@@ -0,0 +1,58 @@
+""" Perform model inference for the jcm model
+
+Derek van Tilborg
+Eindhoven University of Technology
+November 2024
+"""
+
+import os
+import sys
+import warnings
+import pandas as pd
+from constants import ROOTDIR
+from tqdm.auto import tqdm
+from rdkit import Chem
+from rdkit.Chem import RDConfig, QED, Descriptors
+from rdkit import RDLogger
+
+sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
+import sascorer
+
+RDLogger.DisableLog('rdApp.*')
+
+
+if __name__ == '__main__':
+
+    os.chdir(ROOTDIR)
+
+    # read the output file from data_prep_inference.R
+    df = pd.read_csv('results/screening_libraries/all_inference_data.csv')
+
+    smiles = []
+    SA_scores = []
+    MW_scores = []
+    n_atoms = []
+    QED_scores = []
+
+    for smi in tqdm(set(df['smiles'])):
+        try:
+            mol = Chem.MolFromSmiles(smi)
+            sa = sascorer.calculateScore(mol)
+            mw = Descriptors.MolWt(mol)
+            na = mol.GetNumHeavyAtoms()
+            qed = QED.qed(mol)
+
+            smiles.append(smi)
+            SA_scores.append(sa)
+            MW_scores.append(mw)
+            n_atoms.append(na)
+            QED_scores.append(qed)
+        except:
+            pass
+
+    pd.DataFrame({'smiles': smiles,
+                  'SA_scores': SA_scores,
+                  'MW_scores': MW_scores,
+                  'n_atoms': n_atoms,
+                  'QED_scores': QED_scores
+                  }).to_csv('results/screening_libraries/druglike_descriptors.csv', index=False)
