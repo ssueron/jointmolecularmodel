541e433cd16c102e8c547ad8e917e92e22b5ea4b
derekvantilborg <dwvantilborg@gmail.com>
2025-04-02 10:51:37 +0200

    setwd fix

--- plots/scripts/2.0_fig2.R
+++ plots/scripts/2.0_fig2.R
@@ -50,7 +50,7 @@ cols = c('#577788','#97a4ab','#ef9d43','#efc57b', '#578d88', '#99beae')
 #### Load data ####
 
 # Load the data and change some names/factors
-setwd("~/Dropbox/PycharmProjects/JointChemicalModel")
+setwd("~/Dropbox/PycharmProjects/JointMolecularModel")
 
 
 df_2abc <- read_csv('plots/data/df_2abc.csv')
